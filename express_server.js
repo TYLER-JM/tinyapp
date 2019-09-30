7238f5229537c0a91ca600f884d210d2931cfb96
Tyler <t.mar92@gmail.com>
2019-09-30 17:56:37 +0000

    fixed mislabelled folder

--- express_server.js
+++ express_server.js
@@ -20,7 +20,7 @@ app.get('/hello', (req, res) => {
 });
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+  res.render('urls-index', templateVars);
 });
 
 
