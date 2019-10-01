6fe47f1cb66896addd5c16988fe931ed96435c5b
Tyler <t.mar92@gmail.com>
2019-10-01 19:50:44 +0000

    added routes for login logout registration

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
 
+const { users, checkExisting, findUserBy} = require('./data/users');
+//goal is to require an object and access the methods like users.method()
+
 app.use(cookieParser());
 app.set('view engine', 'ejs');
 
@@ -21,6 +24,19 @@ const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabs.ca",
   "9sm5xK" : "http://www.google.com"
 };
+// const users = {
+//   "userRandomID": {
+//     id: "userRandomID",
+//     email: "user@example.com",
+//     password: "purple-monkey-dinosaur"
+//   },
+//   "user2RandomID": {
+//     id: "user2RandomID",
+//     email: "user2@example.com",
+//     password: "dishwasher-funk"
+//   }
+// };
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -37,13 +53,17 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username: req.cookies.username
+    ///CHANGE USERNAME///
+    // username: req.cookies.username
+    user: findUserBy('id', req.cookies.user_id)
   };
   res.render('urls_index', templateVars);
 });
 app.get('/urls/new', (req, res) => {
   let templateVars = {
-    username: req.cookies.username
+    ///CHANGE USERNAME///
+    // username: req.cookies.username
+    user: findUserBy('id', req.cookies.user_id)
   };
   res.render('urls_new', templateVars);
 });
@@ -51,10 +71,29 @@ app.get('/urls/:shortURL', (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies.username
+    ///CHANGE USERNAME///
+    // username: req.cookies.username
+    user: findUserBy('id', req.cookies.user_id)
   };
   res.render('urls_show', templateVars);
 });
+app.get('/register', (req, res) => {
+  let templateVars = {
+    ///CHANGE USERNAME///
+    // username: req.cookies.username
+    user: findUserBy('id', req.cookies.user_id)
+  };
+  res.render('create_account', templateVars);
+});
+
+app.get('/login', (req, res) => {
+  let templateVars = {
+    user: findUserBy('id', req.cookies.user_id)
+  };
+  res.render('login', templateVars);
+});
+
+
 app.post('/urls', (req, res) => {
   let shortened = generateRandomString();
   urlDatabase[shortened] = req.body.longURL;
@@ -74,16 +113,43 @@ app.post('/urls/:id', (req, res) => {
   console.log(req.body.updateLong);
   res.redirect('/urls');
 });
+
+app.post('/register', (req, res) => {
+  let uniqueID = generateRandomString();
+  if (findUserBy('email', req.body.email)) {
+    res.status(400).send('400 error, user email already exists');
+  } else if (!req.body.email || !req.body.password) {
+    res.status(400).send('400 error, both fields are required');
+  } else {
+    users[uniqueID] = {
+      id: uniqueID,
+      email: req.body.email,
+      password: req.body.password
+    };
+  }
+  console.log(users);
+  //set a user_id cookie containing uniqueID
+  res.cookie('user_id', uniqueID);
+  res.redirect('/urls');
+});
+
+//THIS WILL CHANGE//
 app.post('/login', (req, res) => {
   //set a cookie <res.cookie> named username to the value passed in through the form
   //<req.body.username>
   //redirect the browser back to /urls
-  res.cookie('username', req.body.username);
+  let user = findUserBy('email', req.body.email);
+  if (!user) {
+    res.status(403).send("email not found");
+  } else if (user.password !== req.body.password) {
+    res.status(403).send('password incorrect');
+  }
+  res.cookie('user_id', user.id);
   res.redirect('/urls');
-  console.log(req.cookies);
+  // console.log(req.cookies);
 });
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
