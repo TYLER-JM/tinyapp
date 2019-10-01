47bb33869c73ca61650bd223afe05853bef6abcd
Tyler <t.mar92@gmail.com>
2019-10-01 13:45:30 +0000

    added routes to handle login and logout

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const app = express();
 const PORT = 8080;
-
+const express = require('express');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const app = express();
+
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 //copied from stack overflow
@@ -33,14 +35,24 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies.username
+  };
   res.render('urls_index', templateVars);
 });
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  let templateVars = {
+    username: req.cookies.username
+  };
+  res.render('urls_new', templateVars);
 });
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies.username
+  };
   res.render('urls_show', templateVars);
 });
 app.post('/urls', (req, res) => {
@@ -60,6 +72,18 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 app.post('/urls/:id', (req, res) => {
   urlDatabase[req.params.id] = req.body.updateLong;
   console.log(req.body.updateLong);
+  res.redirect('/urls');
+});
+app.post('/login', (req, res) => {
+  //set a cookie <res.cookie> named username to the value passed in through the form
+  //<req.body.username>
+  //redirect the browser back to /urls
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+  console.log(req.cookies);
+});
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
   res.redirect('/urls');
 });
 
